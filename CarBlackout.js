780eb20ce2a6c466f5bf212175a1606a8bd3d82c
canoooooo <58741540+canoooooo@users.noreply.github.com>
2022-07-26 23:48:30 +0200

    changed blackscreen duration to a bit more identic values

--- CarBlackout.js
+++ CarBlackout.js
@@ -2,6 +2,7 @@ import * as alt from 'alt-client';
 import * as game from 'natives';
 
 export default async () => {
+
 };
 
 const lPlayer = alt.Player.local;
@@ -10,19 +11,21 @@ let isBlackedOutAlready = false;
 function blackout() {
     if (!isBlackedOutAlready) {
         isBlackedOutAlready = true;
-        game.doScreenFadeOut(700);
-        setTimeout(() => screenFadeIn(), 1000);
+        game.doScreenFadeOut(100);
+        setTimeout(() => screenFadeIn(), 1500);
         setTimeout(() => isBlackedOutAlready = false, 3000);
     }
 }
 
 function screenFadeIn() {
-    game.doScreenFadeIn(700);
+    game.doScreenFadeIn(700)
 }
 
 function vehicleSpeedCheck() {
     const vehicle = lPlayer.vehicle;
-    let currentSpeedCurrent = (game.getEntitySpeed(vehicle) * 3.6);
+
+    let currentSpeedCurrent = (game.getEntitySpeed(vehicle) * 3.6)
+
     if (currentSpeedCurrent <= 15) {
         blackout();
         game.setVehicleEngineHealth(vehicle, 0);
@@ -32,20 +35,24 @@ function vehicleSpeedCheck() {
 
 function blackoutLogic(oldSpeed) {
     const vehicle = lPlayer.vehicle;
+
     if (game.doesEntityExist(vehicle.scriptID)) {
         let currentSpeed = (game.getEntitySpeed(vehicle) * 3.6);
         if (currentSpeed >= 50) {
             setTimeout(() => vehicleSpeedCheck(), 50);
         }
     }
+
 }
 
 function blackoutCrash() {
     const vehicle = lPlayer.vehicle;
+
     if (vehicle !== null) {
-        const oldSpeed = (game.getEntitySpeed(vehicle) * 3.6);
+        const oldSpeed = (game.getEntitySpeed(vehicle) * 3.6);  
         setTimeout(() => blackoutLogic(oldSpeed), 100);
     }
+
 }
 
 alt.everyTick(blackoutCrash);
